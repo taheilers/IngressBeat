964016a2275c51baf2d3b51fcd746a1479c8a614
theile1083 <theile1083@WS-AC-CTD-37002>
2017-01-26 08:31:52 -0500

    Add project files.

---
+++ IngressBeat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IngressBeat.MvcApplication" Language="C#" %>
---
+++ IngressBeat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IngressBeat.Startup))]
+namespace IngressBeat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
